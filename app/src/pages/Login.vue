1f18ccf147463a610c249344f16fed8d00536777
Antonio Pontes <anvictorsantos@gmail.com>
2021-02-12 08:46:38 -0300

    new - tela de login

--- app/src/pages/Login.vue
+++ app/src/pages/Login.vue
@@ -3,19 +3,18 @@
         <div class="container ass">
             <div class="row">
                 <div class="form-group mt-4 ml-4">
-                    <router-link class="btn btn-white text-blue display-4" to="/home"><i class="fa fa-arrow-left"></i>Voltar a página home</router-link>
+                    <router-link class="btn btn-white text-blue display-4" to="/home">Apoie o Corpo</router-link>
                 </div>
                 <div id="middle-screen" class="col-12 col-md-6 col-lg-4 col-xl-2 text-center">
-                    <form v-on:submit.prevent="register">
+                    <form v-on:submit.prevent="login">
                         <div class="form-group text-center">
                             <h3 class="text-blue text-uppercase font-weight-bold">Seja bem-vindo</h3>
                         </div>
                         <div class="form-group mt-5 mb-4">
-                            <input v-model="email" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                                placeholder="E-mail">
+                            <input v-model="email" type="email" class="form-control" placeholder="E-mail" required>
                         </div>
                         <div class="form-group my-4">
-                            <input v-model="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+                            <input v-model="password" type="password" class="form-control" placeholder="Senha" required>
                         </div>
                         <button class="btn btn-blue text-uppercase text-white font-weight-bold w-75 mt-4 mb-3">Entrar</button>
                     </form>
@@ -40,18 +39,43 @@ export default {
     }
   },
   methods: {
-    register() {
-    axios.post('/api/auth/login', 
+    login() {
+        
+        let loader = this.$loading.show({
+            container: this.fullPage ? null : this.$refs.formContainer,
+            canCancel: false,
+            onCancel: this.onCancel,
+        })
+
+        axios.post('/api/auth/login', 
         {
             email: this.email,
             password: this.password
         })
-        .then((res) => {
-            console.log(res)
-            this.$router.push({ path: '/login' })
+        .then((response) => {
+            
+            console.log(response)
+        
+            setTimeout(() => {
+                loader.hide(),
+                this.$notify({
+                    message: 'Usuário autenticado com sucesso.',
+                    type: 'success'
+                })
+                this.$router.push({ path: '/dashboard' })
+            },1000)   
+            
         })
-        .catch((err) => {
-            console.log(err)
+        .catch((error) => {
+            console.dir(error.response)
+
+                setTimeout(() => {
+                    loader.hide(),
+                    this.$notify({
+                        message: error.response.data.message,
+                        type: 'danger'
+                    })
+                },1000)  
         })
     }
   }
@@ -76,6 +100,12 @@ export default {
         -ms-flex: 0 0 16.666667%;
         flex: 0 0 16.666667%;
         max-width: 18.666667%;
+    }
+}
+
+@media (max-width: 1200px) {
+    #brand {
+        width: 200px;
     }
 }
 </style>
