44fe7e7c258c07ee39c997d2a1aca63550d0a781
Antonio Pontes <anvictorsantos@gmail.com>
2021-05-03 22:31:12 -0300

    fix - ajuste no footer

--- app/src/layout/dashboard/ContentFooter.vue
+++ app/src/layout/dashboard/ContentFooter.vue
@@ -1,13 +1,13 @@
 <template>
   <footer class="footer">
     <div class="container-fluid d-flex flex-wrap justify-content-between">
-      <nav>
+      <!-- <nav>
         <ul>
           <li>
             <router-link :to="{path:'/admin'}">Dashboard</router-link>
           </li>
         </ul>
-      </nav>
+      </nav> -->
       <div class="copyright d-flex flex-wrap">
         &copy; Coded with
         <i class="fa fa-heart heart"></i> by
